d6e5962a01819bd1f53eef854ca3ac2a0f9e84b4
raddevon <raddevon@gmail.com>
2013-02-15 08:18:27 -0500

    Added pseudo-code notes for admin handler

--- admin.py
+++ admin.py
@@ -2,4 +2,7 @@ from users import BaseRequestHandler
 
 class AdminHandler(BaseRequestHandler):
     # todo-devon Write handler for an administration interface
+    # Check for login
+    # If not logged in and if not me, redirect to some other page
+    # If logged in, show a page that allows adding of new issue descriptions
     pass
