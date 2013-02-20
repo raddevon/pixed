6084e9f92b6bb2b3348614e8432a07e0e0146143
raddevon <raddevon@gmail.com>
2013-02-20 09:51:52 -0500

    WTForms installed

--- admin.py
+++ admin.py
@@ -1,8 +1,17 @@
 from users import BaseRequestHandler
+from lib.wtforms.ext.appengine.db import model_form
+from main import PixedPreview
+
+# List of models registered with admin
+admin_models = [
+    PixedPreview,
+]
 
 class AdminHandler(BaseRequestHandler):
     # todo-devon Write handler for an administration interface
     # Check for login
     # If not logged in and if not me, redirect to some other page
     # If logged in, show a page that allows adding of new issue descriptions
-    pass
+    forms = []
+    for model in admin_models:
+        forms.append(model_form(model, ))
